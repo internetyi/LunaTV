1192203cde3e15d65dd0b2870382c4c287eafc71
ZHANGYUAN46 <ZHANGYUAN46@ceair.com>
2026-03-30 17:32:04 +0800

    fixed cloudflare error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 /* eslint-disable @typescript-eslint/no-var-requires */
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+
+export default defineCloudflareConfig({
+  // 使用 R2 作为增量缓存（可选，需要绑定 R2 bucket）
+  incrementalCache: r2IncrementalCache,
+});
 
 const nextConfig = {
   output: 'standalone',
